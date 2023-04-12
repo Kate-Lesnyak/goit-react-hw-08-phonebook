90cc2db37eafafa85763b5b754d2bd71a4876fab
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-04-12 22:52:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import 'modern-normalize';
-import './styles.css';
 
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
